92adc0332d5ee5bb32dfa21cfd4fd3e6905f62a5
d9nich <dan.galaiko@gmail.com>
2020-10-12 19:01:18 +0300

    Animation for slider of cars has been done

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let position = 0
+const NUMBER_OF_POSITIONS = 3;
+const PERCENT_OF_MOVE = -100;
+const movePart = document.getElementById('scroll-cars');
+const ANIMATION_TIME = 1_000 * 10;
+const WAIT_TIME = 1_000 * 5;
+let previous = performance.now();
+let isBack = false;
+
+window.onload = requestAnimationFrame(function scroll(time) {
+    if (previous + WAIT_TIME > time) {
+        if (previous > time) {
+            if (!isBack) {
+                animate(PERCENT_OF_MOVE * ((time - previous) / ANIMATION_TIME + position));
+            } else {
+                animate(PERCENT_OF_MOVE * ((previous - time) / ANIMATION_TIME + position));
+            }
+        } else {
+            animate(PERCENT_OF_MOVE * position);
+        }
+    } else {
+        previous = time + ANIMATION_TIME;
+        animate(PERCENT_OF_MOVE * position);
+        if (isBack) {
+            position -= 1;
+            if (position === -1) {
+                isBack = false;
+                position = 1;
+            }
+        } else {
+            position = position + 1;
+            if (position === NUMBER_OF_POSITIONS) {
+                isBack = true;
+                position = NUMBER_OF_POSITIONS - 2;
+            }
+        }
+    }
+    requestAnimationFrame(scroll);
+});
+
+function animate(percentOfMove) {
+    // console.log(percentOfMove);
+    movePart.setAttribute('style', 'margin-left: ' + percentOfMove + '%');
+}
