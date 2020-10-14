f01e5cd919b2e13c3d589338e4d2cde3d9a922cf
d9nich <dan.galaiko@gmail.com>
2020-10-14 10:58:38 +0300

    Some small warnings has been fixed

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const WAIT_TIME = 1_000 * 5;
 let previous = performance.now();
 let isBack = false;
 
-window.onload = requestAnimationFrame(function scroll(time) {
+window.onload = () => requestAnimationFrame(function scroll(time) {
     if (previous + WAIT_TIME > time) {
         if (previous > time) {
             if (!isBack) {
@@ -62,14 +62,14 @@ function previousCar() {
     animate(PERCENT_OF_MOVE * position);
 }
 
-var lastScrollTop = 0;
+let lastScrollTop = 0;
 const MENU = document.getElementsByTagName('header')[0];
 // element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
 window.addEventListener("scroll", () => { // or window.addEventListener("scroll"....
     if (window.innerWidth > 560) {
         return;
     }
-    var st = window.pageYOffset || document.documentElement.scrollTop;
+    const st = window.pageYOffset || document.documentElement.scrollTop;
     if (st > lastScrollTop) {
         MENU.setAttribute('style', 'top:-30%');
     } else {
